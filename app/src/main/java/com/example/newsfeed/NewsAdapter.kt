32a2511f1f425ae140bc484832adfe650aafae68
Pratyush Singh <aries.pratyush@gmail.com>
2022-09-29 00:33:00 +0530

    First commit

---
+++ app/src/main/java/com/example/newsfeed/NewsAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.newsfeed
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter:RecyclerView.Adapter<NewsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
+class NewsViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+    
+}
